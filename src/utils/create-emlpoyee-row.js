5a59f479493d2bbcc65e89345c0cb7e8ff4baf7f
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2024-07-22 17:50:59 +1000

    Reduces complexity for CSV row processor

---
+++ src/utils/create-emlpoyee-row.js
@@ -0,0 +1,33 @@
+const db = require("./../database/index");
+const OrgChartCSVConsts = require("./org-chart-csv-consts");
+
+const createEmployeeRow = async (
+  row,
+  orgId,
+  jobTitleId,
+  managerId,
+  transaction = null
+) => {
+  const creationParams = {
+    jobId: row[OrgChartCSVConsts.JobId],
+    employeeName: row[OrgChartCSVConsts.EmlpoyeeName],
+    employeeId: parseInt(row[OrgChartCSVConsts.EmployeeId]),
+    emailAddress: row[OrgChartCSVConsts.EmailAddress],
+    rolePriority: parseInt(row[OrgChartCSVConsts.RolePriority]),
+    jobLevel: parseInt(row[OrgChartCSVConsts.JobLevel]),
+    isRoot: row[OrgChartCSVConsts.IsRoot] === "yes" ? true : false,
+    belongsTo: orgId,
+    reportsTo: row[OrgChartCSVConsts.IsRoot] === "yes" ? null : managerId,
+    jobTitleId: jobTitleId,
+  };
+
+  if (transaction) {
+    return await db.accendo.Employee.create(creationParams, {
+      transaction: transaction,
+    });
+  } else {
+    return await db.accendo.Employee.create(creationParams);
+  }
+};
+
+module.exports = createEmployeeRow;
--- src/utils/org-chart-row-processor.js
+++ src/utils/org-chart-row-processor.js
@@ -1,5 +1,7 @@
 const OrgChartCSVConsts = require("./org-chart-csv-consts");
 const db = require("./../database/index");
+const findManager = require("./find-manager");
+const createEmployeeRow = require("./create-emlpoyee-row");
 
 const rowProcessor = async (row, trans, orgId) => {
   let jobTitle = null;
@@ -24,46 +26,20 @@ const rowProcessor = async (row, trans, orgId) => {
 
   // Sort out the manager for the person, it changes from root to no root
   let manager = null;
-  try {
-    if (row[OrgChartCSVConsts.IsRoot] === "no") {
-      // If someone is not a root and without a manager, we have a problem in the organization
-      manager = await db.accendo.Employee.findOne({
-        where: {
-          employeeName: row[OrgChartCSVConsts.ReportsToPerson],
-          jobId: row[OrgChartCSVConsts.ReportsToJobId],
-          belongs_to: orgId,
-        },
-        rejectOnEmpty: true,
-        transaction: trans,
-      });
-    }
-  } catch (e) {
-    // Looks like we found someone without manager in the organization
-    await trans.rollback();
-    throw new Error(
-      "We expected everyone in the organization to have a manager"
+  if (row[OrgChartCSVConsts.IsRoot] === "no")
+    manager = await findManager(
+      row[OrgChartCSVConsts.ReportsToPerson],
+      row[OrgChartCSVConsts.ReportsToJobId],
+      orgId,
+      trans
     );
-  }
 
-  const createdEmployee = await db.accendo.Employee.create(
-    {
-      jobId: row[OrgChartCSVConsts.JobId],
-      employeeName: row[OrgChartCSVConsts.EmlpoyeeName],
-      employeeId: parseInt(row[OrgChartCSVConsts.EmployeeId]),
-      emailAddress: row[OrgChartCSVConsts.EmailAddress],
-      rolePriority: parseInt(row[OrgChartCSVConsts.RolePriority]),
-      jobLevel: parseInt(row[OrgChartCSVConsts.JobLevel]),
-      isRoot: row[OrgChartCSVConsts.IsRoot] === "yes" ? true : false,
-      belongsTo: orgId,
-      reportsTo:
-        row[OrgChartCSVConsts.IsRoot] === "yes"
-          ? null
-          : manager.toJSON().id,
-      jobTitleId: jobTitle.toJSON().id,
-    },
-    {
-      transaction: trans,
-    }
+  const createdEmployee = await createEmployeeRow(
+    row,
+    orgId,
+    jobTitle.toJSON().id,
+    row[OrgChartCSVConsts.IsRoot] === "yes" ? null : manager.toJSON().id,
+    trans
   );
 
   return createdEmployee;
